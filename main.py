9b6a2d8f0d6a85aafc7af51e3c34f139819054b3
Jakex26 <122539748+Jakex26@users.noreply.github.com>
2023-01-31 10:16:39 -0500

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
-basic.show_number(10
-)
+def on_gesture_shake():
+    reset()
+input.on_gesture(Gesture.SHAKE, on_gesture_shake)
+
+def reset():
+    global P1, P2, Ties, Round
+    OLED.init(128, 64)
+    P1 = 0
+    P2 = 0
+    Ties = 0
+    Round = 0
+    OLED.write_string_new_line("Shall We play A game?")
+    basic.pause(2000)
+    OLED.clear()
+    OLED.write_string_new_line("Player1 score:" + str(P1))
+    OLED.new_line()
+    OLED.write_string_new_line("Player2 score:" + str(P2))
+    OLED.new_line()
+    OLED.write_string_new_line("Ties:" + str(Ties))
+    OLED.new_line()
+    OLED.write_string_new_line("Rounds:" + str(Round))
+Round = 0
+Ties = 0
+P2 = 0
+P1 = 0
+reset()
--- main.ts
+++ main.ts
@@ -1,3 +1,37 @@
+function update_score_board () {
+    OLED.clear()
+    OLED.writeStringNewLine("Player1 score:" + P1)
+    OLED.newLine()
+    OLED.writeStringNewLine("Player2 score:" + P2)
+    OLED.newLine()
+    OLED.writeStringNewLine("Ties:" + Ties)
+    OLED.newLine()
+    OLED.writeStringNewLine("Rounds:" + Round)
+}
+input.onButtonPressed(Button.A, function () {
+    OLED.writeStringNewLine("A")
+    basic.pause(1000)
+    OLED.clear()
+    P1 += 1
+    Round += 1
+    update_score_board()
+})
+input.onButtonPressed(Button.AB, function () {
+    OLED.writeStringNewLine("T")
+    basic.pause(1000)
+    OLED.clear()
+    Ties += 1
+    Round += 1
+    update_score_board()
+})
+input.onButtonPressed(Button.B, function () {
+    OLED.writeStringNewLine("B")
+    basic.pause(1000)
+    OLED.clear()
+    P2 += 1
+    Round += 1
+    update_score_board()
+})
 input.onGesture(Gesture.Shake, function () {
     reset()
 })
@@ -9,14 +43,7 @@ function reset () {
     Round = 0
     OLED.writeStringNewLine("Shall We play A game?")
     basic.pause(2000)
-    OLED.clear()
-    OLED.writeStringNewLine("Player1 score:" + P1)
-    OLED.newLine()
-    OLED.writeStringNewLine("Player2 score:" + P2)
-    OLED.newLine()
-    OLED.writeStringNewLine("Ties:" + Ties)
-    OLED.newLine()
-    OLED.writeStringNewLine("Rounds:" + Round)
+    update_score_board()
 }
 let Round = 0
 let Ties = 0
